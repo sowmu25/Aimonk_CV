1a0c5725924481a0e2b9b9f0727c9eafce012275
sowmu25 <sowmu25@gmail.com>
2022-12-10 18:25:23 +0530

    success

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify, render_template
 import os
 from flask_cors import CORS, cross_origin
+import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.models import save_model,load_model
+from tensorflow.keras.preprocessing import image
 import numpy as np
 from predict import Attribute_classify
 from utils import decodeImage
@@ -13,10 +15,7 @@ os.putenv('LC_ALL', 'en_US.UTF-8')
 app = Flask(__name__)
 CORS(app)
 
-"""class ClientApp:
-    def __init__(self):
-        self.filename = "image_974.jpg"
-        self.classifier = Attribute_classify(self.filename)"""
+
 
 model = load_model('model.h5')
 
@@ -28,17 +27,24 @@ def home():
 @app.route("/predict", methods=['POST'])
 @cross_origin()
 def predictRoute():
-    image = request.files['image']
-    test_image = image.load_img(image, target_size=(224, 224,3))
-    test_image = image.img_to_array(test_image)
-    print(test_image)
-    result = model.predict(test_image)
-    result = np.where(result > 0.5, 1, 0)
-    print(result)
-    #test_image = np.expand_dims(test_image, axis = 0)
-    #decodeImage(image, ClientApp().filename)
-    #result = ClientApp().classifier.prediction()
-    return jsonify(result)
+    print("URL HITTED")
+    if request.method == 'POST':
+        upload_file = request.files['fileup']
+        filename = upload_file.filename
+        print(filename)
+        upload_img_path = os.path.join(os.getcwd(),filename)
+        upload_file.save(upload_img_path)
+        test_image = image.load_img(filename, target_size=(224, 224,3))
+        test_image = image.img_to_array(test_image)
+        print(test_image)
+        test_image = test_image/255.
+        #plt.imshow(test_image)
+        test_image = np.expand_dims(test_image, axis=0)
+        result = model.predict(test_image)[0]
+        result = np.where(result > 0.5, 1, 0)
+        print(result)
+      
+        return render_template("index.html",predicted_result="Attribute prediction is {}".format(result))
 
 if __name__ == "__main__":
     #clApp = ClientApp()
